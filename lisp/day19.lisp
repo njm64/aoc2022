a091b55c07ab90c87aa38b64e33e0056b0d40d65
Nick Maher <nick.maher@gmail.com>
2023-01-10 15:29:54 +1100

    Implemented triangle sequence optimisation - 30x speedup!

--- lisp/day19.lisp
+++ lisp/day19.lisp
@@ -1,6 +1,6 @@
-(defpackage :day19
-  (:use :cl :alexandria)
-  (:shadowing-import-from :arrow-macros :->>))
+ (defpackage :day19
+     (:use :cl :alexandria :org.tfeb.hax.memoize)
+     (:shadowing-import-from :arrow-macros :->>))
 
 (in-package :day19)
 
@@ -12,8 +12,8 @@
 
 (defstruct state
   (time 1 :type fixnum)
-  (robots nil :type res)
-  (resources nil :type res))
+  (robots (make-res :ore 1) :type res)
+  (resources (make-res) :type res))
 
 ;; A blueprint is a vector of robots (in order for resources),
 ;; where each robot is a list of the resources required
@@ -55,7 +55,8 @@
 (defun sub-resources (ra rb)
   (decf (res-ore ra) (res-ore rb))
   (decf (res-clay ra) (res-clay rb))
-  (decf (res-obsidian ra) (res-obsidian rb)))
+  (decf (res-obsidian ra) (res-obsidian rb))
+  (decf (res-geode ra) (res-geode rb)))
 
 (defun have-resources (ra rb)
   (and (>= (res-ore ra) (res-ore rb))
@@ -81,37 +82,47 @@
     (2 (incf (res-obsidian r)))
     (3 (incf (res-geode r)))))
 
+(def-memoized-function tri-sequence (n)
+  (if (zerop n) 0 (+ n (tri-sequence (1- n)))))
+
+;; This clever optimisation is from here:
+;; https://www.reddit.com/r/adventofcode/comments/zpihwi/comment/j0tls7a/
+(defun max-possible-geodes (s max-time)
+  (let ((remaining-mins (1+ (- max-time (state-time s)))))
+    (+ (res-geode (state-resources s))
+       (* (res-geode (state-robots s)) remaining-mins)
+       (tri-sequence remaining-mins))))
+
 (defun run-blueprint (bp max-time)
-  (let ((stack (list (make-state :robots (make-res :ore 1) :resources (make-res))))
-        (max-geodes 0)
-        (limits (calc-robot-limits bp)))
-    (loop for s = (pop stack) while s do
-      (maxf max-geodes (count-geodes s max-time))
-      (loop for robot-resources in bp
-            for robot-type = 0 then (1+ robot-type) do
-              (when (and (< (res-n (state-robots s) robot-type)
-                            (res-n limits robot-type))
-                         (has-robots s robot-resources))
-                (let ((time (state-time s))
-                      (robots (copy-res (state-robots s)))
-                      (resources (copy-res (state-resources s))))
-                  (declare (type fixnum time))
-                  (loop until (have-resources resources robot-resources) 
-                        while (< time max-time) do
-                          (add-resources resources robots)
-                          (incf time))
-                  (if (= time max-time)
-                      ;; If we're out of time, just count the maximum number for this state
-                      (maxf max-geodes (count-geodes s max-time))
-                      ;; Otherwise we have enough resources, so build the new robot
-                      (progn
-                        (sub-resources resources robot-resources)
-                        (add-resources resources robots)
-                        (inc-res-n robots robot-type)
-                        (incf time)
-                        (let ((ns (make-state :time time :robots robots :resources resources)))
-                          (push ns stack))))))))
-    max-geodes))
+  (loop with stack = (list (make-state))
+        with limits = (calc-robot-limits bp)
+        with max-geodes = 0
+        finally (return max-geodes)
+        for s = (pop stack) while s
+        when (> (max-possible-geodes s max-time) max-geodes) do
+          (maxf max-geodes (count-geodes s max-time))
+          (loop for robot-resources in bp
+                for robot-type = 0 then (1+ robot-type) do
+                  (when (and (< (res-n (state-robots s) robot-type)
+                                (res-n limits robot-type))
+                             (has-robots s robot-resources))
+                    (let ((time (state-time s))
+                          (robots (copy-res (state-robots s)))
+                          (resources (copy-res (state-resources s))))
+                      (loop until (have-resources resources robot-resources) 
+                            while (< time max-time) do
+                              (add-resources resources robots)
+                              (incf time))
+                      (if (= time max-time)
+                          ;; If we're out of time, just count the maximum number for this state
+                          (maxf max-geodes (count-geodes s max-time))
+                          ;; Otherwise we have enough resources, so build the new robot
+                          (progn
+                            (sub-resources resources robot-resources)
+                            (add-resources resources robots)
+                            (inc-res-n robots robot-type)
+                            (incf time)
+                            (push (make-state :time time :robots robots :resources resources) stack))))))))
 
 (defun part1 (blueprints)
   (loop for bp in blueprints
